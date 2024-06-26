912c1bbde5db258a3db7365a7f5606b49945e7b2
Essam <essamwisam@outlook.com>
2024-06-26 16:57:28 -0500

    ✨ Add OrdinalEncoder

--- src/MLJTransforms.jl
+++ src/MLJTransforms.jl
@@ -5,15 +5,28 @@ using CategoricalArrays
 using MLJModelInterface: MLJModelInterface
 using TableOperations
 using MLJBase
+using StatsBase
+
 const MMI = MLJModelInterface
 
+# Functions of generic use across transformers
 include("generic.jl")
+
 # Target encoding
 include("target_encoding/errors.jl")
 include("target_encoding/target_encoding.jl")
-export target_encoding_fit, transformit
 include("target_encoding/interface_mlj.jl")
-export TargetEncoder
+export target_encoder_fit, target_encoder_transform, TargetEncoder
+
+# Ordinal encoding
+include("ordinal_encoding/ordinal_encoding.jl")
+include("ordinal_encoding/interface_mlj.jl")
+export ordinal_encoder_fit, ordinal_encoder_transform, OrdinalEncoder
+
+# Frequency encoding
+include("frequency_encoding/frequency_encoding.jl")
+include("frequency_encoding/interface_mlj.jl")
+export frequency_encoder_fit, frequency_encoder_transform, FrequencyEncoder
 
 
 end
---
+++ src/ordinal_encoding/interface_mlj.jl
@@ -0,0 +1,159 @@
+### TargetEncoding with MLJ Interface
+
+# 1. Interface Struct
+mutable struct OrdinalEncoder{ AS <: AbstractVector{Symbol}} <: Unsupervised
+	cols::AS
+	exclude_cols::Bool
+	encode_ordinal::Bool
+end;
+
+# 2. Constructor
+function OrdinalEncoder(;
+	cols = Symbol[],
+	exclude_cols = true,
+	encode_ordinal = false,
+)
+    return OrdinalEncoder(cols, exclude_cols, encode_ordinal)
+end;
+
+
+
+# 4. Fit result structure (what will be sent to transform)
+struct OrdinalEncoderResult <: MMI.MLJType
+	# target statistic for each level of each categorical column
+	index_given_col_level::Dict{Symbol, Dict{Any, Any}}
+end
+
+# 5. Fitted parameters (for user access)
+MMI.fitted_params(::OrdinalEncoder, fitresult) = (
+	index_given_col_level = fitresult.index_given_col_level,
+)
+
+# 6. Fit method
+function MMI.fit(transformer::OrdinalEncoder, verbosity::Int, X)
+	fit_res =  ordinal_encoder_fit(
+		X,
+		transformer.cols;
+		exclude_cols = transformer.exclude_cols,
+		encode_ordinal = transformer.encode_ordinal,
+	)
+	fitresult = OrdinalEncoderResult(
+		fit_res[:index_given_col_level],
+	)
+	report = Dict(:encoded_cols => fit_res[:encoded_cols])        # report only has list of encoded columns
+	cache = nothing
+	return fitresult, cache, report
+end;
+
+
+# 7. Transform method
+function MMI.transform(transformer::OrdinalEncoder, fitresult, Xnew)
+	fit_res = Dict(
+		:index_given_col_level =>
+			fitresult.index_given_col_level,
+	)
+	Xnew_transf = ordinal_encoder_transform(Xnew, fit_res)
+	return Xnew_transf
+end
+
+# 8. Extra metadata
+MMI.metadata_pkg(
+	OrdinalEncoder,
+	name = "MLJTransforms",
+	package_uuid = "23777cdb-d90c-4eb0-a694-7c2b83d5c1d6",
+	package_url = "https://github.com/JuliaAI/MLJTransforms.jl",
+	is_pure_julia = true,
+)
+
+MMI.metadata_model(
+	OrdinalEncoder,
+    input_scitype = Table(Union{Infinite, Finite}),
+    output_scitype =  Table(Union{Infinite, Finite}),
+	load_path = "MLJTransforms.OrdinalEncoder" 
+)
+
+
+
+
+"""
+$(MMI.doc_header(OrdinalEncoder))
+
+`OrdinalEncoder` implements ordinal encoding which replaces the categorical values in the specified
+	categorical columns with integers (ordered arbitrarily).
+
+In MLJ (or MLJModels) do `model = OrdinalEncoder()` which is equivalent to `model = OrdinalEncoder(cols = Symbol[],
+	exclude_cols = true,
+	encode_ordinal = false, )` to construct a model instance.
+
+# Training data
+
+In MLJ (or MLJBase) bind an instance unsupervised `model` to data with
+
+    mach = machine(model, X)
+
+Here:
+
+- `X` is any table of input features (eg, a `DataFrame`). Categorical columns in this table must have
+    scientific types `Multiclass` or `OrderedFactor` for their elements.
+
+Train the machine using `fit!(mach, rows=...)`.
+
+# Hyper-parameters
+
+- `cols=[]`: A list of names of categorical columns given as symbols to exclude or include from encoding
+- `exclude_cols=true`: Whether to exclude or includes the columns given in `cols`
+- `encode_ordinal=false`: Whether to encode `OrderedFactor` or ignore them
+
+# Operations
+
+- `transform(mach, Xnew)`: Apply target encoding to the`Multiclass` or `OrderedFactor` selected columns of `Xnew` and return the new table. 
+    Columns that are not `Multiclass` or `OrderedFactor` will be always left unchanged.
+
+# Fitted parameters
+
+The fields of `fitted_params(mach)` are:
+
+- `index_given_col_level`: A dictionary that maps each level for each column in a subset of the categorical columns of X into an integer. 
+
+# Examples
+
+```julia
+using ScientificTypes
+using MLJTransforms
+using MLJ
+
+# Define categorical columns
+A = ["g", "b", "g", "r", "r",]  
+B = [1.0, 2.0, 3.0, 4.0, 5.0,]
+C = ["f", "f", "f", "m", "f",]  
+D = [true, false, true, false, true,]
+E = [1, 2, 3, 4, 5,]
+
+# Combine into a named tuple
+X = (A = A, B = B, C = C, D = D, E = E)
+
+# Coerce A, C, D to multiclass and B to continuous and E to ordinal
+X = coerce(X,
+:A => Multiclass,
+:B => Continuous,
+:C => Multiclass,
+:D => Multiclass,
+:E => OrderedFactor,
+)
+
+encoder = OrdinalEncoder(encode_ordinal = false)
+mach = fit!(machine(encoder, X))
+Xnew = transform(mach, X)
+
+julia > Xnew
+	(A = [2, 1, 2, 3, 3],
+	B = [1.0, 2.0, 3.0, 4.0, 5.0],
+	C = [1, 1, 1, 2, 1],
+	D = [2, 1, 2, 1, 2],
+	E = CategoricalArrays.CategoricalValue{Int64, UInt32}[1, 2, 3, 4, 5],)
+```
+
+See also
+[`TargetEncoder`](@ref)
+"""
+OrdinalEncoder
