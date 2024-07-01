8254437c59373ac0f5f70c17708049893c2417c7
Essam <essamwisam@outlook.com>
2024-07-01 17:58:28 -0500

    🚑 Replace name with package_name

--- src/ordinal_encoding/interface_mlj.jl
+++ src/ordinal_encoding/interface_mlj.jl
@@ -50,7 +50,7 @@ end
 # 8. Extra metadata
 MMI.metadata_pkg(
     OrdinalEncoder,
-    name = "MLJTransforms",
+    package_name = "MLJTransforms",
     package_uuid = "23777cdb-d90c-4eb0-a694-7c2b83d5c1d6",
     package_url = "https://github.com/JuliaAI/MLJTransforms.jl",
     is_pure_julia = true,
